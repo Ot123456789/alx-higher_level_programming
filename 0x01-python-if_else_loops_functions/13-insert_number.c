c1b4fa050dbeac1ea4c8165823247a9eac7e0e4d
Ot123456789 <otmaneelouafi@gmail.com>
2024-01-03 05:38:38 -0800

    up

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,38 +2,36 @@
 #include <stdlib.h>
 
 /**
- * insert_node - inserts node in sorted list
- * @head: address of head pointer
- * @number: number to insert
- * Return: inserted node
- */
+* insert node — inserts node in sorted list
+* @head: address of head pointer
+* @number: number to insert
+* Return: inserted node
+*/
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *node = *head, *new = malloc(sizeof(listint_t));
+        listint_t *node = *head, *new = malloc(sizeof(listint_t));
 
-    if (!new)
-        return (NULL);
+        if (!new)
+                return (NULL);
 
-    new->n = number;
-    new->next = NULL;
+        new->n = number;
+        new->next = NULL;
 
-    if (!node || new->n < node->n)
-    {
-        new->next = node;
-        *head = new;
-        return (*head);
-    }
-
-    while (node)
-    {
-        if (!node->next || new->n < node->next->n)
+        if (!node || new->n < node->n)
         {
-            new->next = node->next;
-            node->next = new;
-            return (new);
+                new->next = node;
+                return (*head = new);
         }
-        node = node->next;
-    }
 
-    return (NULL);
+        while (node)
+        {
+                if (!node->next || new->n < node->next->n)
+                {
+                        new->next = node->next;
+                        node->next = new;
+                        return (node);
+                }
+                node = node->next;
+        }
+	return (NULL);
 }
