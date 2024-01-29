56f1184c0f1090b0aab900f9b2c82e133ef7a034
Ot123456789 <otmaneelouafi@gmail.com>
2024-01-29 06:51:12 -0800

    up

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Defining an empty class Rectangle """
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
-    """Empty representation of a Rectamgle"""
+    """Represent a rectangle."""
     pass
