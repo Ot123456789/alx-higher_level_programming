3136152cc2245708f66161493e71934be5a543ff
Ot123456789 <otmaneelouafi@gmail.com>
2024-01-30 07:10:05 -0800

    up

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,72 +1,58 @@
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
+import sys
 
-from sys import argv
-
-def print_solution(board):
-    """Prints the solution in a readable format"""
-    for row in board:
-        print(row)
-
-def is_valid(board, row, col, n):
-    """Checks if it's valid to place a queen at board[row][col]"""
-    # Check this row on left side
-    for i in range(col):
-        if board[row][i] == 1:
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
             return False
 
-    # Check upper diagonal on left side
+    # Check upper left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, n), range(col, -1, -1)):
+    # Check upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
         if board[i][j] == 1:
             return False
 
     return True
 
-def solve_n_queens_util(board, col, n):
-    """Recursive utility function to solve N Queens problem"""
-    if col >= n:
-        return True
+def solve_nqueens(board, row, N, solutions):
+    if row == N:
+        solutions.append([[i, col] for i, col in enumerate(board)])
+        return
 
-    for i in range(n):
-        if is_valid(board, i, col, n):
-            board[i][col] = 1
-            if solve_n_queens_util(board, col + 1, n):
-                return True
-            board[i][col] = 0
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solve_nqueens(board, row + 1, N, solutions)
+            board[row][col] = 0
 
-    return False
+def print_solutions(solutions):
+    for sol in solutions:
+        print(sol)
 
-def solve_n_queens(n):
-    """Solves the N Queens problem and prints the solution"""
-    if n < 4:
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
         print("N must be at least 4")
-        return
+        sys.exit(1)
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-
-    if solve_n_queens_util(board, 0, n) is False:
-        print("Solution does not exist")
-        return
-
-    print_solution(board)
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve_nqueens(board, 0, N, solutions)
+    print_solutions(solutions)
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    
-    if not argv[1].isdigit():
-        print("N must be a number")
-        exit(1)
-    
-    n = int(argv[1])
-    solve_n_queens(n)
-
+    main()
