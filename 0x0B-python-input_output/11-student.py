3b0e06ef9a681a914a4814f5694c3ce62625b74c
Ot123456789 <otmaneelouafi@gmail.com>
2024-02-07 14:15:39 -0800

    up

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,9 +3,9 @@
 
 
 class Student:
-    '''student class'''
+    '''Student class'''
     def __init__(self, first_name, last_name, age):
-        '''Initialization of the student object'''
+        '''Initialization of the Student object'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
