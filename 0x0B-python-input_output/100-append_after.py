8d52dbc5bac65bd8de4c6c31e90aacd8a468595c
Ot123456789 <otmaneelouafi@gmail.com>
2024-02-07 14:38:14 -0800

    up

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Contains the append_after function"""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Appends 'new_string' after a line containing
     'search_string' in 'filename'."""
