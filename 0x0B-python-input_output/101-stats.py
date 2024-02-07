e4d49b3f04efa9c79b98d1f9d03c7bfa121f82f8
Ot123456789 <otmaneelouafi@gmail.com>
2024-02-07 14:36:38 -0800

    up

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,25 +3,28 @@
 
 from sys import stdin
 
+
 status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+        }
 
 total_size = i = 0
+
 
 def printer():
     '''This function prints the statistics'''
     print(f'File size: {total_size}')
     for key, value in sorted(status_codes.items()):
         if value > 0:
-            print('{:s} : {:d}'.format(key, value))
+            print('{:s}: {:d}'.format(key, value))
+
 
 try:
     for line in stdin:
@@ -31,7 +34,6 @@ try:
             total_size += int(splitted_line[-1])
             if status in status_codes:
                 status_codes[status] += 1
-
         i += 1
 
         if i % 10 == 0:
