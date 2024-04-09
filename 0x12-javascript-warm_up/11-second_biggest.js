a25c1dcfc0f1b562f2a24a3aca2cf55d4a6bbe41
Ot123456789 <otmaneelouafi@gmail.com>
2024-04-08 17:32:24 -0700

    up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+
 const args = process.argv.slice(2).map(Number);
+
 if (args.length <= 1) {
   console.log(0);
 } else {
-  const sortedArgs = args.sort((a, b) => b - a);
-  const secondBiggest = sortedArgs[1];
+  args.sort((a, b) => b - a);
+  const secondBiggest = args[1];
   console.log(secondBiggest);
 }
